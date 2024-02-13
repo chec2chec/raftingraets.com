6dc044502776f2c3c2dc32e7a98fc84ad0f5efc5
chec2chec <ivansimeonoff@gmail.com>
2024-02-14 01:44:26 +0200

    layout fix and thumbnail added

--- wp-content/themes/raftingraets/index.php
+++ wp-content/themes/raftingraets/index.php
@@ -4,19 +4,19 @@
     <div class="container py-5">
         <div class="row">
             <div class="col-lg-12">
-            <div class="row pb-3">
-            <?php while (have_posts()) : the_post(); ?>
-                
-                <?php
-                    if (is_single()) {
-                        get_template_part('template-parts/content', 'single');
-                    } else {
-                        get_template_part('template-parts/content', 'list');
-                    }
-                ?>
-            <?php
-                endwhile; // End of the loop.
-            ?>
+                <div class="row pb-3">
+                    <?php while (have_posts()) : the_post(); ?>
+                        <?php
+                            if (is_single()) {
+                                get_template_part('template-parts/content', 'single');
+                            } else {
+                                get_template_part('template-parts/content', 'list');
+                            }
+                        ?>
+                    <?php
+                        endwhile; // End of the loop.
+                    ?>
+                </div>
             </div>
 
             <div class="col-12">
